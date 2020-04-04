200e1d9a13c7152d9ca05503288624951622fb87
Sxy623 <191436989@qq.com>
2020-04-04 20:59:50 +0800

    Add players

---
+++ player/AI_player.py
@@ -0,0 +1,39 @@
+class AIPlayer:
+    """
+    AI 玩家
+    """
+
+    def __init__(self, color):
+        """
+        玩家初始化
+        :param color: 下棋方，'X' - 黑棋，'O' - 白棋
+        """
+
+        self.color = color
+
+    def get_move(self, board):
+        """
+        根据当前棋盘状态获取最佳落子位置
+        :param board: 棋盘
+        :return: action 最佳落子位置, e.g. 'A1'
+        """
+        if self.color == 'X':
+            player_name = '黑棋'
+        else:
+            player_name = '白棋'
+        print("请等一会，对方 {}-{} 正在思考中...".format(player_name, self.color))
+
+        # -----------------请实现你的算法代码--------------------------------------
+
+        # 用 list() 方法获取所有合法落子位置坐标列表
+        action_list = list(board.get_legal_actions(self.color))
+
+        # 如果 action_list 为空，则返回 None,否则从中选取一个随机元素，即合法的落子坐标
+        if len(action_list) == 0:
+            action = None
+        else:
+            action = action_list[0]
+        
+        # ------------------------------------------------------------------------
+
+        return action
